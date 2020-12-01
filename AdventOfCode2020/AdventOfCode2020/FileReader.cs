2522f622e434f6e35fdbbf0bba1d5931c3c65d9c
ShawnPence <45135345+ShawnPence@users.noreply.github.com>
2020-12-01 14:02:53 -0600

    add additional file read methods

--- AdventOfCode2020/AdventOfCode2020/FileReader.cs
+++ AdventOfCode2020/AdventOfCode2020/FileReader.cs
@@ -33,5 +33,55 @@ namespace AdventOfCode2020
 			}
 			return inputtext;
 		}
+
+		public static List<int> ReadLinesInt(string fileName = "")
+		{
+			var inputtext = new List<int>();
+			if (fileName == "")
+			{
+				Console.WriteLine("Input file:");
+				fileName = Console.ReadLine();
+			}
+			while (!File.Exists(fileName))
+			{
+				Console.WriteLine("file not found - try again:");
+				fileName = Console.ReadLine();
+			}
+
+			using (StreamReader sr = new StreamReader(fileName))
+			{
+				while (sr.Peek() != -1)
+				{
+					inputtext.Add(Convert.ToInt32(sr.ReadLine()));
+				}
+
+			}
+			return inputtext;
+		}
+
+		public static List<long> ReadLinesLong(string fileName = "")
+		{
+			var inputtext = new List<long>();
+			if (fileName == "")
+			{
+				Console.WriteLine("Input file:");
+				fileName = Console.ReadLine();
+			}
+			while (!File.Exists(fileName))
+			{
+				Console.WriteLine("file not found - try again:");
+				fileName = Console.ReadLine();
+			}
+
+			using (StreamReader sr = new StreamReader(fileName))
+			{
+				while (sr.Peek() != -1)
+				{
+					inputtext.Add(Convert.ToInt64(sr.ReadLine()));
+				}
+
+			}
+			return inputtext;
+		}
 	}
 }
