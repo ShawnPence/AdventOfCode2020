48469e8120219ec697c05acd7aabe2dfc996e1d1
ShawnPence <45135345+ShawnPence@users.noreply.github.com>
2020-11-30 18:36:06 -0600

    Create project for Advent of Code 2020 and add file reader class

---
+++ AdventOfCode2020/AdventOfCode2020/FileReader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+	class FileReader
+	{
+		public static List<string> ReadLines(string fileName = "")
+		{
+			var inputtext = new List<string>();
+			if (fileName == "")
+			{
+				Console.WriteLine("Input file:");
+				fileName = Console.ReadLine();
+			}
+			while (!File.Exists(fileName))
+			{
+				Console.WriteLine("file not found - try again:");
+				fileName = Console.ReadLine();
+			}
+
+			using (StreamReader sr = new StreamReader(fileName))
+			{
+				while (sr.Peek() != -1)
+				{
+					inputtext.Add(sr.ReadLine());
+				}
+
+			}
+			return inputtext;
+		}
+	}
+}
