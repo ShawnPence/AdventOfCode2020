4654c2b53d441a24e56ce98bb611056da41902f5
ShawnPence <45135345+ShawnPence@users.noreply.github.com>
2020-12-03 00:19:09 -0600

    add extension methods for converting string to integer arrays

---
+++ AdventOfCode2020/AdventOfCode2020/Extensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+	/// <summary>
+	/// extension methods that may be useful for sovling puzzles quickly
+	/// </summary>
+	static class Extensions
+	{
+		public static int[] ToIntArray(this string s, char d = ',')
+		{
+			return s.Split(d).Select(x => Convert.ToInt32(x)).ToArray();
+		}
+
+		public static int[] ToIntArray(this string s, char[] d)
+		{
+			return s.Split(d).Select(x => Convert.ToInt32(x)).ToArray();
+		}
+
+		public static long[] ToLongArray(this string s, char d = ',')
+		{
+			return s.Split(d).Select(x => Convert.ToInt64(x)).ToArray();
+		}
+
+		public static long[] ToLongArray(this string s, char[] d)
+		{
+			return s.Split(d).Select(x => Convert.ToInt64(x)).ToArray();
+		}
+	}
+}
