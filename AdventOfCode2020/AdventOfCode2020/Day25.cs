633b49089aba9fb38a69be264e0d4fc20504e9bb
ShawnPence <45135345+ShawnPence@users.noreply.github.com>
2020-12-25 12:59:05 -0600

    Day 25

---
+++ AdventOfCode2020/AdventOfCode2020/Day25.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+	class Day25
+	{
+		public static void Problems()
+		{
+			var input = FileReader.ReadLinesLong("Inputs\\day25.txt");
+
+			long number = 1;
+			long repeat = 0;
+			while(number != input[0])
+			{
+				number = (number * 7) % 20201227;
+				repeat++;
+			}
+			number = 1;
+			for(int i = 1; i <= repeat; i++)
+			{ 
+				number = (number * input[1]) % 20201227;
+			}
+			Console.WriteLine(number);
+		}
+	}
+}
--- AdventOfCode2020/AdventOfCode2020/Program.cs
+++ AdventOfCode2020/AdventOfCode2020/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2020
 	{
 		static void Main(string[] args)
 		{
-			Day24.Problems();
+			Day25.Problems();
 
 
 		}
